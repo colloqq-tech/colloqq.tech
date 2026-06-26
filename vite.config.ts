9b4974a914703c9149ba4d7c023cfd25a6232679
ik4rd <treep.me@yandex.ru>
2026-06-26 22:10:13 +0300

    fix(ik4rd): deploy issue

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		host: true,
-	},
+    plugins: [react()],
+    server: {
+        host: true,
+    },
+    build: {
+        outDir: ".",
+        emptyOutDir: false,
+    },
 });
